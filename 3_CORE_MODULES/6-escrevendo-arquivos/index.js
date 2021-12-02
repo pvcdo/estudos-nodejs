e15bba8987bbaf4e4089f406fc8d70a77015672e
Paulo Oliveira <paulocivictor@gmail.com>
2021-12-02 00:34:13 -0300

    escrevendo, atualizando e removendo arquivos via fs

--- 3_CORE_MODULES/6-escrevendo-arquivos/index.js
+++ 3_CORE_MODULES/6-escrevendo-arquivos/index.js
@@ -12,16 +12,26 @@ const server = http.createServer((req, res) => {
 
   if (!name) {
     fs.readFile("index.html", function (err, data) {
+      //data é o texto do arquivo lido
       res.writeHead(200, { "Content-Type": "text/html" });
       res.write(data);
       return res.end();
 
       /*
-        Caso a url não tenha uma query que contenha um parâmetro name, o arquivo index.html é lido, armazenado na memória e seu conteúdo é escrito na tela. No index.html há um formulário que ao ser preenchido recarrega a página enviando uma requisição com uma query com um parâmetro name cujo valor é a resposta ao formulário. No ato da página ser recarregada é constatado a existência de query.name e portanto caímos no else.
+        Caso a url passada no navegador para abrir a página não tenha uma query que contenha um parâmetro name, o arquivo index.html é lido, armazenado na memória e seu conteúdo é escrito na tela. 
+        
+        No index.html há um formulário que ao ser preenchido recarrega a página enviando uma requisição com uma query com um parâmetro name cujo valor é a resposta ao formulário. No ato da página ser recarregada é constatado a existência de query.name e portanto caímos no else.
       */
     });
   } else {
-    fs.writeFile("arquivo.txt", name, function (err, data) {
+    /*fs.writeFile("arquivo.txt", name, function (err, data) {
+      res.writeHead(302, {
+        Location: "/",
+      });
+      return res.end();
+    });*/
+
+    fs.appendFile("arquivo.txt", name + ',\r\n' , function (err, data) {
       res.writeHead(302, {
         Location: "/",
       });
@@ -29,7 +39,11 @@ const server = http.createServer((req, res) => {
     });
 
     /* 
-        Ao ser encontrado o query.name é escrito um arquivo.txt contendo no valor de name, além disso é criado o cabeçalho de resposta com o status 302.
+        Ao ser encontrado o query.name é escrito um arquivo.txt contendo no valor de name, além disso é criado o cabeçalho de resposta com o status 302 e a página é recarregada sem nenhuma tag query. Tudo isso é feito com o comando fs.writeFile. Esse comando porém substitui tudo o que estava anteriormente escrito nesse arquivo.
+
+        O comando fs.appendFile, porém, adiciona textos ao arquivo, atualizando seu valor.
+
+           \n\r é uma tag de quebra de linha compatível para windows e linux 
     */
   }
 });
