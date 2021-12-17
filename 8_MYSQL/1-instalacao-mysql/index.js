a36018df08bf82c36fd361db659f0ee5e66b61c0
Paulo Oliveira <paulocivictor@gmail.com>
2021-12-17 00:44:19 -0300

    instalacao workbench ok, conexão com banco ok, fazer alteração da senha do localhost do mysql

---
+++ 8_MYSQL/1-instalacao-mysql/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const exphbs = require('express-handlebars')
+const mysql = require('mysql')
+
+//const senhaMySQL = require('./conn')
+
+const app = express()
+
+//app.use(express.static('public'))
+
+app.engine('handlebars',exphbs.engine())
+app.set('view engine','handlebars')
+
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Password',
+    database: 'nodemysql',
+})
+
+conn.connect((err)=>{
+    if(err){
+        console.log(err)
+        return
+    }
+    console.log('Conectou ao MySQL')
+    app.listen(3000, ()=>{
+        console.log('App rodando na porta 3000')
+    })
+})
