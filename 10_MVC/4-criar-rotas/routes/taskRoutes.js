988b699f36619ce525a5524c998e2e5bbd6a28e7
PAULO VICTOR CAETANO DE OLIVEIRA PR114067 <paulocivictor@gmail.com>
2022-03-28 11:54:25 -0300

    aulas de salvar dados e resgatar dados a partir da arquitetura MVC

--- 10_MVC/4-criar-rotas/routes/taskRoutes.js
+++ 10_MVC/4-criar-rotas/routes/taskRoutes.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router()
 const TaskController = require('../controllers/TaskController')
 
-router.get('/add', TaskController.createTask())
-router.get('/', TaskController.showTasks())
+router.get('/add', TaskController.createTask)
+router.get('/', TaskController.showTasks)
 
 
 module.exports = router;
